304481fb709e554830365ff5f6bf863ef1bf4f83
billchen1977 <billchen1977@gmail.com>
2017-12-27 09:01:20 +0100

    drivers: clk: add custom voltage control
    @AshishM94 : ported to msm8916

--- drivers/clk/qcom/clock-a7.c
+++ drivers/clk/qcom/clock-a7.c
@@ -171,6 +171,62 @@ static void populate_opp_table(struct platform_device *pdev)
 	print_opp_table(a7_cpu);
 }
 
+#ifdef CONFIG_ARCH_MSM8916
+extern int cpr_regulator_get_corner_voltage(struct regulator *regulator,
+		int corner);
+extern int cpr_regulator_set_corner_voltage(struct regulator *regulator,
+		int corner, int volt);
+
+ssize_t cpu_clock_get_vdd(char *buf)
+{
+	ssize_t count = 0;
+	int i, uv;
+
+	if (!buf)
+		return 0;
+
+	for (i = 1; i < a7ssmux.c.num_fmax; i++) {
+		uv = cpr_regulator_get_corner_voltage(
+					a7ssmux.c.vdd_class->regulator[0],
+					a7ssmux.c.vdd_class->vdd_uv[i]);
+		if (uv < 0)
+			return 0;
+		count += sprintf(buf + count, "%lumhz: %d mV\n",
+					a7ssmux.c.fmax[i] / 1000000,
+					uv / 1000);
+	}
+
+	return count;
+}
+
+ssize_t cpu_clock_set_vdd(const char *buf, size_t count)
+{
+	int i, mv, ret;
+	char line[32];
+
+	if (!buf)
+		return -EINVAL;
+
+	for (i = 1; i < a7ssmux.c.num_fmax; i++) {
+		ret = sscanf(buf, "%d", &mv);
+		if (ret != 1)
+			return -EINVAL;
+
+		ret = cpr_regulator_set_corner_voltage(
+					a7ssmux.c.vdd_class->regulator[0],
+					a7ssmux.c.vdd_class->vdd_uv[i],
+					mv * 1000);
+        if (ret < 0)
+			return ret;
+
+        ret = sscanf(buf, "%s", line);
+		buf += strlen(line) + 1;
+	}
+
+	return count;
+}
+#endif
+
 static int of_get_fmax_vdd_class(struct platform_device *pdev, struct clk *c,
 								char *prop_name)
 {
