2e5d1b06804da634ecf827d9c7a85b27519142ff
BlackMesa <brother12@hotmail.it>
2018-01-23 17:53:25 +0100

    build_quasar: bump to v1.6

--- build_quasar.sh
+++ build_quasar.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Coded by BlackMesa @QuasarKernel v1
+# Coded by BlackMesa @QuasarKernel
 clear
-QS_VERSION=v1.5
+QS_VERSION=v1.6
 QS_DATE=$(date +%Y%m%d)
 QS_TOOLCHAIN=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/UBERTC-arm-eabi-4.8/bin/arm-eabi-
+QS_JOBS=`grep processor /proc/cpuinfo|wc -l`
 echo "------------------------------------------"
 echo "QuasarKernel $QS_VERSION Build Script"
 echo "Coded by BlackMesa"
 echo "------------------------------------------"
-PS3='Please select the kernel variant you want to build (1-4): '
-options=("j53g" "j5lte" "j5nlte" "j5ylte" "Exit")
+PS3='Please select the kernel variant you want to build (1-6): '
+options=("j53g" "j5lte" "j5nlte" "j5x3g" "j5xlte" "j5ylte" "Exit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -26,8 +27,8 @@ do
             make clean
             rm -r -f output
             mkdir output
-            make -C $(pwd) O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j53g_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
-            make -C $(pwd) O=output
+            make -C $(pwd) -j$QS_JOBS O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j53g_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
+            make -C $(pwd) -j$QS_JOBS O=output
             mv $(pwd)/output/arch/arm/boot/zImage $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-zImage
             $(pwd)/tools/dtbTool -o $(pwd)/quasar/dtb.img $(pwd)/output/arch/arm/boot/dts/
             mv $(pwd)/quasar/dtb.img $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-dtb
@@ -56,8 +57,8 @@ do
             make clean
             rm -r -f output
             mkdir output
-            make -C $(pwd) O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5lte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
-            make -C $(pwd) O=output
+            make -C $(pwd) -j$QS_JOBS O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5lte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
+            make -C $(pwd) -j$QS_JOBS O=output
             mv $(pwd)/output/arch/arm/boot/zImage $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-zImage
             $(pwd)/tools/dtbTool -o $(pwd)/quasar/dtb.img $(pwd)/output/arch/arm/boot/dts/
             mv $(pwd)/quasar/dtb.img $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-dtb
@@ -86,8 +87,68 @@ do
             make clean
             rm -r -f output
             mkdir output
-            make -C $(pwd) O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5nlte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
-            make -C $(pwd) O=output
+            make -C $(pwd) -j$QS_JOBS O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5nlte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
+            make -C $(pwd) -j$QS_JOBS O=output
+            mv $(pwd)/output/arch/arm/boot/zImage $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-zImage
+            $(pwd)/tools/dtbTool -o $(pwd)/quasar/dtb.img $(pwd)/output/arch/arm/boot/dts/
+            mv $(pwd)/quasar/dtb.img $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-dtb
+            $(pwd)/quasar/ramdisk/$QS_VARIANT/repackimg.sh
+            mv $(pwd)/quasar/ramdisk/$QS_VARIANT/ramdisk-new.cpio.gz $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-ramdisk.cpio.gz
+            mv $(pwd)/quasar/ramdisk/$QS_VARIANT/image-new.img $(pwd)/quasar/build/boot-$QS_VARIANT-$QS_DATE.img
+            echo " "
+            echo "------------------------------------------"
+            echo "Kernel build finished."
+            echo "boot.img is located into quasar/build."
+            echo "Press any key for end the script."
+            echo "------------------------------------------"
+            read -n1 -r key
+            break
+            ;;
+        "j5x3g")
+            clear
+            echo "------------------------------------------"
+            echo "Building kernel for j5x3g..."
+            echo "------------------------------------------"
+            echo " "
+            QS_VARIANT=j5x3g
+            export ARCH=arm
+            export CROSS_COMPILE=$QS_TOOLCHAIN
+            export LOCALVERSION=-Quasar_Kernel-$QS_VERSION-$QS_VARIANT-$QS_DATE
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) -j$QS_JOBS O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5x3g_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
+            make -C $(pwd) -j$QS_JOBS O=output
+            mv $(pwd)/output/arch/arm/boot/zImage $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-zImage
+            $(pwd)/tools/dtbTool -o $(pwd)/quasar/dtb.img $(pwd)/output/arch/arm/boot/dts/
+            mv $(pwd)/quasar/dtb.img $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-dtb
+            $(pwd)/quasar/ramdisk/$QS_VARIANT/repackimg.sh
+            mv $(pwd)/quasar/ramdisk/$QS_VARIANT/ramdisk-new.cpio.gz $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-ramdisk.cpio.gz
+            mv $(pwd)/quasar/ramdisk/$QS_VARIANT/image-new.img $(pwd)/quasar/build/boot-$QS_VARIANT-$QS_DATE.img
+            echo " "
+            echo "------------------------------------------"
+            echo "Kernel build finished."
+            echo "boot.img is located into quasar/build."
+            echo "Press any key for end the script."
+            echo "------------------------------------------"
+            read -n1 -r key
+            break
+            ;;
+        "j5xlte")
+            clear
+            echo "------------------------------------------"
+            echo "Building kernel for j5xlte..."
+            echo "------------------------------------------"
+            echo " "
+            QS_VARIANT=j5xlte
+            export ARCH=arm
+            export CROSS_COMPILE=$QS_TOOLCHAIN
+            export LOCALVERSION=-Quasar_Kernel-$QS_VERSION-$QS_VARIANT-$QS_DATE
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) -j$QS_JOBS O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5xlte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
+            make -C $(pwd) -j$QS_JOBS O=output
             mv $(pwd)/output/arch/arm/boot/zImage $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-zImage
             $(pwd)/tools/dtbTool -o $(pwd)/quasar/dtb.img $(pwd)/output/arch/arm/boot/dts/
             mv $(pwd)/quasar/dtb.img $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-dtb
@@ -116,8 +177,8 @@ do
             make clean
             rm -r -f output
             mkdir output
-            make -C $(pwd) O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5ylte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
-            make -C $(pwd) O=output
+            make -C $(pwd) -j$QS_JOBS O=output quasar_msm8916_defconfig VARIANT_DEFCONFIG=quasar_msm8916_j5ylte_defconfig SELINUX_DEFCONFIG=quasar_selinux_defconfig
+            make -C $(pwd) -j$QS_JOBS O=output
             mv $(pwd)/output/arch/arm/boot/zImage $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-zImage
             $(pwd)/tools/dtbTool -o $(pwd)/quasar/dtb.img $(pwd)/output/arch/arm/boot/dts/
             mv $(pwd)/quasar/dtb.img $(pwd)/quasar/ramdisk/$QS_VARIANT/split_img/boot.img-dtb
